4967220a75593030fd1a5751b099f1e1a8a0549f
Jorden Lowe <supermitsuba@gmail.com>
2015-02-20 17:13:32 -0500

    Added scaffolding for tests i want to write

---
+++ RAMLSharp.Test/RamlMapperResponseBodyTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RAMLSharp.Test
+{
+    [TestClass]
+    public class RamlMapperResponseBodyTests
+    {
+        [TestMethod]
+        public void RamlMapper_CreateOneResponseBody_GenerateRAML()
+        {
+
+        }
+
+        [TestMethod]
+        public void RamlMapper_CreateTwoResponseBody_GenerateRAML()
+        {
+
+        }
+
+        [TestMethod]
+        public void RamlMapper_CreateNullResponseBody_GenerateRAML()
+        {
+
+        }
+
+        [TestMethod]
+        public void RamlMapper_CreateEmptyResponseBody_GenerateRAML()
+        {
+
+        }
+
+        [TestMethod]
+        public void RamlMapper_CreateNullContentType_GenerateRAMLWithoutResponse()
+        {
+
+        }
+
+        [TestMethod]
+        public void RamlMapper_CreateNullExample_GenerateRAMLWithoutExample()
+        {
+
+        }
+
+        [TestMethod]
+        public void RamlMapper_CreateNullStatusCode_GenerateRAMLWithoutStatusCode()
+        {
+
+        }
+
+        [TestMethod]
+        public void RamlMapper_CreateNullDescription_GenerateRAMLWithoutDescription()
+        {
+
+        }
+    }
+}
